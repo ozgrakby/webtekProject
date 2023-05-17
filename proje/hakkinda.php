0669975b9e5298a432ca12146e44f6ae5ec52bd4
ozgrakby <131009455+ozgrakby@users.noreply.github.com>
2023-05-17 15:57:37 +0300

    hakkinda.php güncelleme
    -Title tagı eklendi.
    -Navbarda düzeltme yapıldı.
    -Sayfanın içeriği eklendi.
    -Bazı tagların id'leri eklendi.

--- proje/hakkinda.php
+++ proje/hakkinda.php
@@ -5,8 +5,9 @@
     <link rel="stylesheet" href="hakkinda.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-</head>
-<body style="margin-right:50px; margin-left:50px;">
+    <title>Hakkında</title>
+    </head>
+<body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mx-5 mt-3 rounded-pill">
         <div class="container-fluid">
             <a class="navbar-brand" href="./hakkinda.php">Hakkında</a>
@@ -25,7 +26,6 @@
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Şehrim</a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <li><a class="dropdown-item" href="./sehrimiz.php">Şehrimiz</a></li>
-                    <li><a class="dropdown-item" href="./takimimiz.php">Takımımız</a></li>
                     <li><a class="dropdown-item" href="./mirasimiz.php">Mirasımız</a></li>
                 </ul>
                 </li>
@@ -35,24 +35,22 @@
                 <li class="nav-item"><a class="nav-link" href="./giris.php">Giriş</a></li>
             </ul>
         </div>
-    </nav>
-        
-    <section class="container" style="margin-top:30px">
+    </nav> 
+    <section class="container">
         <section class="row">
-            <div class="col" style="display: flex; align-items: center; justify-content: center;" >
-                <img src="ben.jpg" alt="kendi fotom" style="width:192px;height:192px;">
+            <div class="col" id="divIMG">
+                <img src="ben.jpg" id="benIMG" alt="kendi fotom">
             </div>
             <article class="col-8">
-                <h1> Özgür ben </h1>
-                <p>&nbsp;&nbsp;&nbsp;&nbsp;Özgür ben aaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaaaaa aaaaaaaaaaa aaaaaa aaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaa</p>
+                <h1> Burası Nedir? </h1>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; Özgür ben, Kütahyalıyım. Sakarya Üniversitesi Bilgisayar Mühendisliği Bölümünün bir öğrencisiyim. Bu sitede benim hakkımda ve Kütahya hakkında bilgiler bulunmaktadır.</p>
             </article>
         </section>
         <br><br>
         <article class="row">
-            <h1> Özgür ben </h1>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;Özgür ben aaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaaaaa aaaaaaaaaaa aaaaaa aaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaaaaaa aaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaa</p>
+            <h1> Ben </h1>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp; Hayatı oldukça yavaş ve monoton olan biri olduğum için çeşit çeşit hobi ve ilgi alanlarım bulunmamaktadır. Hayatımın büyük bir bölümü bilgisayar oyunlarında geçti ve geçmektedir. Onun yanında doğal olarak hayatımda yazılım da büyük bir yer kaplamaktadır.</p>
         </article>
     </div>
-        
 </body>
 </html>
