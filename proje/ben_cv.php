6be1dfbc882ca8b3c25691916e4c738057190499
ozgrakby <131009455+ozgrakby@users.noreply.github.com>
2023-05-17 16:44:19 +0300

    ben_cv.php güncelleme
    Özgeçmiş Sayfasına;
    -Title eklendi.
    -İçerik eklendi.
    -Bazı elemanlarının id'leri eklendi.

--- proje/ben_cv.php
+++ proje/ben_cv.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="ben_cv.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <title>Özgeçmiş</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mx-5 mt-3 rounded-pill">
@@ -38,18 +39,36 @@
     <div class="container" style="margin-top:30px">
         <div class="row">
             <div class="col-4">
-                <div>
-                    <img src="ben.jpg" alt="kendi fotom" style="width:192px;height:192px;">
+                <div id="divIMG">
+                    <img src="vesika.jpg" id="vesikaIMG" alt="vesikalık" style="width:192px;height:288px;">
                 </div>
-                <article>
-                    <h1>baslık1<h1>
-                    <p>metin1</p>
+                <article id="solSutunT">
+                    <h4>İletişim</h4>
+                    <hr>
+                    <p><b>Mail:</b>akbayozgur2@gmail.com</p>
+                    <p><b>Discord:</b>ozgr#9696</p>
+                    <h4>Kişisel Bilgiler</h4>
+                    <hr>
+                    <p><b>Doğum Tarihi:</b>12.01.2004</p>
+                    <p><b>Medeni Hali:</b>Bekar</p>
+                    <p><b>Ehliyet:</b>-yok-</p>
+                    <p><b>Askerlik:</b>-yapılmadı-</p>
                 </article>
             </div>            
             <div class="col-8">
-                <article>
-                    <h1>baslik2<h1>
-                    <p>metin2</p>
+                <h1>Özgür Akbay</h1>
+                <article id="sagSutunT">
+                    <h4>Bilgisayar Mühendisliği</h4><hr>
+                    <article id="sagAltT">
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp; Bir bilgisayar mühensiliği öğrencisi olarak proje yönetimi ve planlı çalışma konusunda kendimi geliştimek için çabalamaktayım. C/C++, C# alanlarında belli bir ilerleme sahibiyim. Ayrıca web tasarım ve python alanlarında kendimi geliştirmeye çalışıyorum. </p>
+                    </article>
+                    <h4>Eğitim Bilgisi</h4><hr>
+                    <article id="sagAltT">
+                    <h5>Lise</h5><hr>
+                    <p>Tavşanlı Fen Lisesi 2018-2022<p>
+                    <h5>Lisans</h5><hr>
+                    <p>Sakarya Üniversitesi Bilgisayar Mühendisliği 2022-<p>
+                    </article><hr>
                 </article>
             </div>
         </div>
