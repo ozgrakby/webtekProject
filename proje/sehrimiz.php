13081a8c21415797154e2514d25f014a07f83c0c
ozgrakby <131009455+ozgrakby@users.noreply.github.com>
2023-05-19 22:24:20 +0300

    sehrimiz.php güncelleme
    -Title eklendi.
    -Slider için bir div ve o div altında başka dört div eklendi.
    -Dört alt div için gerekli anchor ve img elemanları eklendi.
    -Slider'ın çalışması için gerekli olan script eklendi.

--- proje/sehrimiz.php
+++ proje/sehrimiz.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="sehrimiz.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <title>Şehrim</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mx-5 mt-3 rounded-pill">
@@ -25,7 +26,6 @@
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Şehrim</a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <li><a class="dropdown-item" href="./sehrimiz.php">Şehrimiz</a></li>
-                    <li><a class="dropdown-item" href="./takimimiz.php">Takımımız</a></li>
                     <li><a class="dropdown-item" href="./mirasimiz.php">Mirasımız</a></li>
                 </ul>
                 </li>
@@ -36,45 +36,43 @@
             </ul>
         </div>
     </nav>
-<?php
+        
+    <div id="sliderContainer">
+        <div class="slide">
+            <a href="#"><img src="slide1.jpg" class="slideIMG"></a>
+        </div>
+        <div class="slide">
+            <a href="#"><img src="slide2.jpg" class="slideIMG"></a>
+        </div>
+        <div class="slide">
+            <a href="#"><img src="slide3.jpg" class="slideIMG"></a>
+        </div>
+        <div class="slide">
+            <a href="#"><img src="slide4.jpg" class="slideIMG"></a>
+        </div>
 
-?>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
-
+        <a id="arrowPrev" onclick="plusSlides(-1)">&lArr;</a>
+        <a id="arrowNext" onclick="plusSlides(1)">&rArr;</a>
+    </div>
+        
 </body>
+    <script>
+        let slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+        showSlides(slideIndex += n);
+        }
+
+        function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("slide");
+        if (n > slides.length) {slideIndex = 1}    
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slides[slideIndex-1].style.display = "block";  
+        }
+    </script>
 </html>
