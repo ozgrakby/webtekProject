316808d80ce196152dcdf7c0ed4712cb7986b94c
ozgrakby <131009455+ozgrakby@users.noreply.github.com>
2023-05-21 14:58:40 +0300

    sehrimiz.php güncelleme
    -Sliderdaki fotoğrafların adları slider'ın altına eklendi. Bunun için script de güncellendi.
    -Semantic taglar kullanılarak sayfanın içeriği eklendi.

--- proje/sehrimiz.php
+++ proje/sehrimiz.php
@@ -37,35 +37,69 @@
         </div>
     </nav>
         
-    <div id="sliderContainer">
+    <main id="sliderContainer">
         <div class="slide">
-            <a href="#"><img src="slide1.jpg" class="slideIMG"></a>
+            <a href="#vazoText"><img src="slide1.jpg" class="slideIMG"></a>
         </div>
         <div class="slide">
-            <a href="#"><img src="slide2.jpg" class="slideIMG"></a>
+            <a href="#ulucText"><img src="slide2.jpg" class="slideIMG"></a>
         </div>
         <div class="slide">
-            <a href="#"><img src="slide3.jpg" class="slideIMG"></a>
+            <a href="#grmskText"><img src="slide3.jpg" class="slideIMG"></a>
         </div>
         <div class="slide">
-            <a href="#"><img src="slide4.jpg" class="slideIMG"></a>
+            <a href="#hisarText"><img src="slide4.jpg" class="slideIMG"></a>
         </div>
-
+        
         <a id="arrowPrev" onclick="plusSlides(-1)">&lArr;</a>
         <a id="arrowNext" onclick="plusSlides(1)">&rArr;</a>
-    </div>
+    </main>
+        <div id="sliderCaption"></div>
+    <main>
+        <h1>Kütahya</h1>
+        <section class="container">
+            <article class="row">
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; Ege Bölgesi'nde bulunan <b>Kütahya</b>'nın il nüfusu yaklaşık 580 bindir. Merkez ilçe nüfusu ise yaklaşık 270 bindir. <b>Kütahya</b> 11.634 km² ile Türkiye'nin yüz ölçümünü bakımından 25. en büyük ilidir. <b>Kütahya</b>'ın yüzölçünün %56'sı ormandır, bu veride Türkiye'de yüzölçümüne oranla orman varlığı açısından 5. sıradadır.</p><br>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; <b>Kütahya</b>'nın kuruluş tarihi kesin olmamakla birlikte İlk Tunç Çağına kadar dayanmaktadır. Bu topraklara yerleşen en eski topluluk Friglerdir. Kütahya'da Frig varlığı MÖ 1200'lere dayanmaktadır. MÖ 676 yılında Kimmerler, Frigleri mağlup ederek <b>Kütahya</b> ve Çervresine egemen oldular. Daha sonra Lidyalılara geçen <b>Kütahya</b>, MÖ 546 yılında Perslerin Anadolu'yu istila etmesi sonucu el değiştirmişti. MÖ 334 yılında İskender'in Persleri yenmesi ile yöreyi egemenlik altına aldı. MÖ 133 yılında Roma yönetimine giren <b>Kütahya</b> daha sonra Piskoposluk merkezi haline gelmiştir.</p><br>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; 1071 Malazgirt Meydan muharebesi sonrası Alparslan'a yenilen Diyojen esirliğin sonra <b>Kütahya</b>'ya getirilip gözleri kör edilmiştir. 1078'de Anadolu Selçuklu Devletinin kurucusu Kutalmışoglu Süleyman Şah <b>Kütahya</b>'yı ele geçirmiştir. Daha sonra şehir Bizans, Haçlılar ve Selçuklu Devleti arasında el değiştirip durmuştur. 1277'den sonra şehir Germiyanoğlu Beyliğinin egemenliğine girmiştir. 1381'de gerçekleşen Yıldırım Bayezid ile Devlet Hatun evliliği sonucu Kütahya ve çevresi Osmalılara çeyiz olarak verilmiştir. 1402 Ankara savaşından sonra Anadolu Timur tarafından istila edildiğinde Kütahya II. Yakup Beye verilmiş ve daha sonra şehir geri Osmalılara geçmiştir.</p><br>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; <b>Kütahya</b>'da çini örnekleri 14.yüzyıl sonlarında görülmeye başlanmıştır. Çinide asıl ilerleme İznik'in çini sanatının zirvesinde olduğu 1550'li yıllardan sonra başlamıştır. 18. yüzyılda İznik'te çiniciliğin tamamen kaybolmasından sonra Osmalı'da tek yer <b>Kütahya</b> kalmıştır.</p><br>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; <b>Kütahya</b>'nın yemek kültürünün temelini buğday ürünleri, hamurlular ve süt ürünleri oluşturur. Hamurlara örnek olarak; cimcik, kıymalı-mercimekli mantı, tirit, haşhaşlı pide, dolamber böreği, şibit gösterilebilir. Sıkıcık -tarhana ve bulgur küçük yuvarlak köftelerle yapılır-, düğün çorbası, tutmaç, tarhana, paça çorbası çorbalara örnek göstirelebilir. Labada dolması, lahana sarması, soğan dolması, göbeç(güveç) yine <b>Kütahya</b>'nın yemeklerindendir.</p><br>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; Aizanoi <b>Kütahya</b>'da gezilecek yerlerin başında gelmektedir <a href="./mirasimiz.php">bu linkten</a> Aizanoi hakkında bilgiye ulaşabilirsiniz. Daha sonra Altıntaş ve Dumlupınar ilçelerinin çevreleri her bir karışı gezmeye görmeye değerdir. Buralar Kurtuluş Savaşının en kanlı çarpışmalarının yaşandığı noktalardır. Dumlupınar Şehitliği, Zafertepeçalköydeki Zafer Anıtı görülmelidir. Macar Evi de yine <b>Kütahya</b>'da görülmesi gereken bir yerdir. 19. yüzyıl ortalarında Macar Özgürlük Savaşı önderlerinden Lajos Kossuth'un Osmanlı Devletine sığındığı sırada yaşadığı konaktır. Arkeoloji Müzesi, Jeoloji Müzesi, Çini Müzesi şehrin önemli müzeleridir yine buralarda gezilmeye görülmeye değerdir.</p><br>
+            </article>
+            <section class="row">
+                <article class="col-sm col-lg-3" id="vazoText">
+                    <h4>Vazo</h4>
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp; <b>Kütahya</b>'da önemli bir yeri olan Vazo, Zafer Meydanında bulunur. Merkezi yerinden ötürü birçok adres Vazo'ya göre tarif edilir. 2022-2023 yıllarında boyunca meydan yenilenmiş ve Vazo restorasyondan geçirilirek bir kaç metre taşınmıştır.</p>
+                </article>
+                <article class="col-sm col-lg-3" id="ulucText">
+                    <h4>Ulu Camii</h4>
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp; <b>Kütahya</b>'nın en büyük ve en güzel camisidir. Yıldırım Bayezid Camii olarak da bilinir. Yakınında bulunana Döneneler Mevlevihanesi de görülmeye değerdir.</p>
+                </article>
+                <article class="col-sm col-lg-3" id="grmskText">
+                    <h4>Germiyan Sokak</h4>
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp; <b>Kütahya</b>'nın günümüze kalmayı başarmış konaklarının en iyi örnekleri bu sokak ve çevresinde bulunur. Bazı konaklar müzeye çevrilmiştir ve eski yaşam hakkında bilgi vermektedir.</p>
+                </article>
+                <article class="col-sm col-lg-3" id="hisarText">
+                    <h4>Kütahya Kalesi</h4>
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp; MS 8. yüzyılda Bizans tarafından inşa edilmiştir. Buraya egemen olmuş her güç burayı kullanmış ve ilave yapmışlardır. Hisar tam ortasında Döner gazino olarak bilinen 45 dakikada 360 derece dönen bir restoran bulunmaktadır. Halk arasında "Kütahya Kalesi" ifadesi nerdeyse hiç kullanılmaz herkes buraya <b>Hisar</b> der.</p>
+                </article>
+            </section>
+        </section>
+    </main>
         
 </body>
-    <script>
-        let slideIndex = 1;
-        showSlides(slideIndex);
+<script>
+    let slideIndex = 1;
+    let caption = document.getElementById("sliderCaption");
+    const text = ["Vazo", "Ulu Camii", "Germiyan Sokak", "Hisar"];
+    showSlides(slideIndex);
 
-        function plusSlides(n) {
+    function plusSlides(n) {
         showSlides(slideIndex += n);
-        }
+    }
 
-        function showSlides(n) {
-        let i;
+    function showSlides(n) {
+        let i; 
         let slides = document.getElementsByClassName("slide");
         if (n > slides.length) {slideIndex = 1}    
         if (n < 1) {slideIndex = slides.length}
@@ -73,6 +107,7 @@
             slides[i].style.display = "none";  
         }
         slides[slideIndex-1].style.display = "block";  
-        }
-    </script>
+        caption.innerHTML = "<p><b>" + text[slideIndex-1] + "</b></p>";
+    }
+</script>
 </html>
