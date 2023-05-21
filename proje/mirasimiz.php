a6eab3eebaaa62d0105e9cf0c8566b1be4f21bb7
ozgrakby <131009455+ozgrakby@users.noreply.github.com>
2023-05-21 14:54:17 +0300

    mirasimiz.php güncelleme
    -Title eklendi.
    -Gerekli semantic taglar eklendi.
    -Semantic taglar altında içerikler eklendi.

--- proje/mirasimiz.php
+++ proje/mirasimiz.php
@@ -5,9 +5,10 @@
     <link rel="stylesheet" href="mirsasimiz.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <title>Mirasımız</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mx-5 mt-3 rounded-pill">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mx-5 mt-3">
         <div class="container-fluid">
             <a class="navbar-brand" href="./hakkinda.php">Hakkında</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,7 +26,6 @@
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Şehrim</a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <li><a class="dropdown-item" href="./sehrimiz.php">Şehrimiz</a></li>
-                    <li><a class="dropdown-item" href="./takimimiz.php">Takımımız</a></li>
                     <li><a class="dropdown-item" href="./mirasimiz.php">Mirasımız</a></li>
                 </ul>
                 </li>
@@ -36,45 +36,26 @@
             </ul>
         </div>
     </nav>
-<?php
-
-?>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
- <p>a</p><br>
-
+    <main>
+        <section class="container">
+            <section class="row">
+                <section class="col-sm col-lg-6" id="divIMG">
+                    <img src="aizanoi.jpg" id="mirasIMG">
+                </section>
+                <article class="col-sm col-lg-6">
+                    <h1>Aizanoi</h1>
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp; Kütahya'nın Çavdarhisar ilçesinde bulunan bir antik kenttir <b>Aizanoi</b>. Etimolojik olarak antik kentin adı Zeus'un kızlarından erato ile Arkadia kralı Arkas'ın oğlu Azan'ın kenti olduğundan geldiği düşünülmektedir.</p>
+                </article>
+            </section>
+            <hr>
+            <article class="row">
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; Antik kent, Frigya'ya bağlı yaşayan Aizanitis'lerin ana yerleşim merkeziydi. Kentin yüksek platosu üzerinde bulunan Zeus Tapınağı'nın çevresinde yapılan kazılarda İlk Tunç Çağına dayanan yerleşim izleri ortaya çıkmıştır.</p><br>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; Bu bölge MÖ 133'de Roma egemnliğine girmiştir. <b>Aizanoi</b> Roma döneminde tahıl ekimi, yün ve şarap üretimi sayesinde zenginleşmiş ve ünlenmiştir. Bu bölgede ilk sikkelerin MÖ 1. yüzyılın sonlarında basıldığı bilinmektedir. <b>Aizanoi</b>, en parlak dönemini MS 2. yüzyılda yaşamıştır. Bu dönemde büyük imar faaliyetleri dörmüştür.</p><br>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; Erken Bizans Dönemi'nde <b>Aizanoi</b>, psikoposluk merkezi iken, MS 7. yüzyıldan sonra önemini yitirmiştir. Tapınak düzlüğü Orta Çağ'da bir hisara dönüştürülmüştür. Selçuklu egemnliğinde ise <b>Aizanoi</b>, Çavdar Tatarları tarafından üs olarak kullanılmasından dolayı buraya Çavdarhisar adı verilmiştir.</p><br>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; <b>Aizanoi</b>, 19. yüzyılın ilk çeyreğinde Avrupalı gezginler tarafından keşfedilmiş, daha sonra incelenmiş ve tanımlanmıştır. 1926 yılında yapılan ilk kazılar sonucu yapılan çalışmalar Alman arkeolog Dr. Daniel Krencker tarafından yayınlanmıştır. 1970'den bu yana kazı çalışmaları devam etmektedir.</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; <b>Aizanoi</b> antik kentinde; Zeuz tapınağı, tiyatro, stadyum, iki hamam, dünyanın ilk ticaret borsa binası, sütunlu cadde, Kocaçay üzerinde beş köprü -ikisi ayaktadır-, iki agora, nekropoller, antik bir bent ve su yolları bulunmaktadır.</p>
+            </diarticle>
+        </section>
+    </main>
 </body>
 </html>
